4fd8c998e6c3315b05f0a58e77ebdaf43c17a582
Mingyoung Lai <mingyounglai@Mingyoungs-MacBook-Pro.local>
2023-09-11 12:11:33 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,31 +4,53 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            i = dofizzbuzzw(i);
+        }
+    }
 
-        for (int i = 1; i < 100; i++) {
+    private static int dofizzbuzzw(int i) {
+        boolean dby3 = i % 3 == 0;
+        boolean dby5 = i % 5 == 0;
+        if (dby3 && dby5){
+            System.out.println("Fizz Buzz");
+        }
+        else if (dby3){
+            System.out.println("Fizz");
+        }
+        else if (dby5){
+            System.out.println("Buzz");
+        }
+        else System.out.println(i);
+        i++;
+        return i;
+    }
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+    private static void fizzbuzzf(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-                System.out.println("Fizz Buzz");
+            System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy3) {
+        } else if (divisibleBy3) {
 
-                System.out.println("Fizz");
+            System.out.println("Fizz");
 
-            } else if (divisibleBy5) {
+        } else if (divisibleBy5) {
 
-                System.out.println("Buzz");
+            System.out.println("Buzz");
 
-            } else {
+        } else {
 
-                System.out.println(i);
+            System.out.println(i);
 
-            }
         }
     }
 }
+
+
